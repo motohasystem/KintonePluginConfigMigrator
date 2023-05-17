2dbb9b173f65e0bb04b5d8b47b6248142ba5a8d5
daisuke motohashi <ddssk.m@gmail.com>
2023-05-17 13:10:00 +0900

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true,
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": ["error", 4]
+    },
+    "globals": {
+        "kintone": true
+    }
+}
---
+++ src/config-migrator.js
@@ -0,0 +1,222 @@
+// import { Utils } from "commonutils"
+import { Button } from 'kintone-ui-component/lib/button'
+import { Utils} from 'kintoneplugin-commonutils'
+
+export class ConfigMigrator {
+    LABEL_ITEM = '📁 プラグイン設定を保存・復元します'
+    LABEL_IMPORT_BUTTON = '⬆️設定をアップロード（復元）'
+    LABEL_EXPORT_BUTTON = '⬇️設定をダウンロード（保存）'
+    HEADER_NODE_ID = "KINTANLAB-SETTING-IMPORTER"
+    ID_SUBMIT_BUTTON = 'button-load-settings'
+
+    /** @type {Record<string, string>} */
+    store_config = {}
+
+    constructor(plugin_id) {
+        this.plugin_id = plugin_id
+    }
+
+    /**
+   * インポート・エクスポートフォームを配置します。
+   * put_forms関数はプラグインIDを使用してエクスポートフォームを作成し、インポートフォームも作成します。
+   * 要素を構築するUtils.buildElementを使用し、ヘッダーにラベルと要素を追加します。
+   */
+    put_forms() {
+        const form_export = this.compose_export_form(this.plugin_id)
+        const form_import = this.compose_import_form()
+
+        const forms = Utils.buildElement({
+            tagName: 'div'
+            , className: 'container_import_export'
+            , childElements: [
+                form_export
+                , form_import
+            ]
+        })
+
+        const label = Utils.buildElement({
+            tagName: 'div'
+            , textContent: this.LABEL_ITEM
+        })
+        const header = Utils.ce('div', '', [label])
+
+        const node_header = document.getElementById(this.HEADER_NODE_ID)
+        if (node_header == null) {
+            return
+        }
+        node_header.appendChild(
+            Utils.ce('div', '', [
+                header
+                , forms
+            ])
+        )
+
+    }
+
+    // インポートフォームの構築
+    compose_import_form  () {
+        const button_import = this.make_button_import()
+
+        return Utils.buildElement({
+            tagName: 'div'
+            , className: 'import_form_block'
+            , childElements: [
+                button_import
+            ]
+            , attrs: {
+                'style': 'margin-top: 1em;'
+            }
+        })
+    }
+
+    // インポートボタンの構築
+    make_button_import() {
+        const btn_import = new Button({
+            'text': this.LABEL_IMPORT_BUTTON
+            , 'type': 'submit'
+            , 'id': this.ID_SUBMIT_BUTTON
+        })
+
+        const file_input = Utils.buildElement({
+            tagName: 'input'
+            , attrs: {
+                type: 'file'
+                , id: 'fileInput'
+                , value: this.LABEL_IMPORT_BUTTON
+            }
+        })
+
+
+        // インポートボタンのclickイベント
+        // eslint-disable-next-line no-unused-vars
+        btn_import.addEventListener('click', (_event) => {
+            file_input.click();
+        })
+
+        // ファイル選択フォームのchange（ファイル選択）イベント
+        file_input.addEventListener('change', (event) => {
+            this.importSettings(event)
+        })
+
+        return btn_import
+    }
+
+    /**
+   * ファイル選択イベントを受け取って、ファイル読み込みとJSONデータをプラグイン設定として保存する処理を行う
+   * @param {Event} event - イベントオブジェクト
+   */
+    importSettings(event) {
+        if (event == null || event.target == null) {
+            return
+        }
+        // @ts-ignore
+        const file = event.target.files[0];
+        if (!file) {
+            return;
+        }
+
+        const reader = new FileReader();
+
+        reader.onload = (event) => {
+            try {
+                if (event.target == null) {
+                    throw new Error('event.targetにnullが返りました。')
+                }
+                if (typeof event.target.result != 'string') {
+                    throw new Error('event.target.result が文字列型ではありませんでした。')
+                }
+                const jsonData = JSON.parse(event.target.result);
+                console.log(jsonData);
+                // JSONデータを変数に読み込んだ後の処理をここで実行
+                this.store_config = jsonData
+
+                // 最終確認ダイアログと読み込みの実行
+                const msg = '現在の設定を上書きして、読み込んだ設定を保存します。よろしいですか？'
+                if (confirm(msg)) {
+                    kintone.plugin.app.setConfig(this.store_config, function () {
+                        alert('🆙 プラグイン設定を保存しました。アプリの更新をお忘れなく！');
+                        window.location.href = '../../flow?app=' + kintone.app.getId();
+                    });
+                }
+
+
+            } catch (error) {
+                console.error('ファイルの読み込みに失敗しました:', error);
+            }
+        };
+
+        reader.readAsText(file);
+    }
+
+    /**
+   * 指定されたプラグインのIDを持つエクスポートリンクを生成し、ヘッダノードに追加します
+   * 
+   * @param {string} [plugin_id] プラグインのID。省略時は this.plugin_id が使用される
+   * @throws {Error} ヘッダノードが見つからない場合
+   */
+    put_export_link(plugin_id) {
+        if (plugin_id == undefined) {
+            plugin_id = this.plugin_id
+        }
+
+        // 設定のロード
+        const btn_link = this.compose_export_form(plugin_id)
+        const node_header = document.getElementById(this.HEADER_NODE_ID)
+        if (node_header == null) {
+            throw new Error(`ノード #${this.HEADER_NODE_ID} が見つかりませんでした。`)
+        }
+        node_header.appendChild(btn_link)
+    }
+
+    /**
+   * plugin_idを受け取り、設定情報をJSON形式でダウンロードするボタンを生成します。
+   * @param {String} plugin_id プラグインID
+   * @return {HTMLElement} 生成されたHTML要素
+   */
+    compose_export_form(plugin_id) {
+        const CONF = kintone.plugin.app.getConfig(plugin_id);
+        const serialized = JSON.stringify(CONF)
+        const btn_link = this.make_download_button(serialized, 'setting.json')
+        const form = Utils.ce('div', 'export_button_block', [btn_link], '', {
+            'style': 'float: left; margin-right: 1em;'
+        })
+        return form
+    }
+
+    /**
+   * ダウンロードボタンを構築します。
+   * 
+   * @param {Uint8Array} data - データ
+   * @param {string} filename - ファイル名 (デフォルト: settings.txt)
+   * @returns {HTMLElement} 構築したブロック要素
+   */
+    make_download_button(data, filename = 'settings.txt') {
+        const bom = new Uint8Array([0xEF, 0xBB, 0xBF]); // add BOM
+        const blob = new Blob([bom, data], { type: "octet/stream" })
+
+        const anchor = Utils.buildElement({
+            tagName: 'a'
+            , attrs: {
+                href: window.URL.createObjectURL(blob)
+                , download: filename
+                , role: 'button'
+            }
+        })
+
+        // エクスポートボタン
+        const button = new Button({
+            'text': this.LABEL_EXPORT_BUTTON
+            , 'type': 'normal'
+        })
+
+        // クリックイベント
+        button.addEventListener('click', (event) => {
+            console.log(event);
+            anchor.click();
+        })
+
+        const block = Utils.ce('div', '', [button])
+        return block
+    }
+
+}
